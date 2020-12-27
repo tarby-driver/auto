835fd1aa1e8494da3fe0bc76f4c3042efb83bf38
panicros <panicarosa@gmail.com>
2020-12-27 17:06:40 -0600

    new test2

---
+++ shop-web-tests/shop-web-tests/UnitTest2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace shop_web_tests
+{
+    //[TestClass] - for NUnit need to write TestFixture
+    [TestFixture]
+    public class UnitTest2
+    {
+        [Test] //- for NUnit need to write Test
+        public void TestMethod1()
+        {
+        }
+    }
+}
